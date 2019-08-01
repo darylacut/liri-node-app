7c69cc1efa7bc650c11148031e3a67d49f27595c
Daryl Acut <darylacut@yahoo.com>
2019-08-01 00:55:16 -0400

    defined searchConcert function

--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-//grabs the axios package
+//grabs the axios package;
 var axios = require( "axios" );
 
 //reading and setting variables with the dotenv package;
@@ -7,16 +7,23 @@ require( "dotenv" ).config();
 //imports keys.js file and stores it as a variable;
 var keys = require( "./keys.js" );
 
-//enables access to Spotify keys
+//enables access to Spotify keys;
 var spotify = new Spotify( keys.spotify );
 
-//assigns the user's input and searh process to global variables
-    //var UserInput = process.argv[3];
-    //var SearchProcess = process.argv[2];
+//assigns the user's input and searh process to global variables;
+    var SearchProcess = process.argv[2];
+    var UserInput;
+    // we need to loop through the node array for all the words of the artist's name;
+    for (var i = 3; i < process.argv.length; i++) {
+        if (i > 3 && i < process.argv.length) {
+        UserInput = UserInput + "%20" + process.argv[i];
+        } else {
+            UserInput = process.argv[i];
+    }
 
-//this creates a switch case statement depending on what the user searches for
-// we also assign which function to run depending on the process
-switch (process.argv[2]) {
+//this creates a switch case statement depending on what the user searches for;
+// we also assign which function to run depending on the process;
+switch (SearchProcess) {
     case "concert-this": searchConcert();
         break;
     
@@ -32,6 +39,20 @@ switch (process.argv[2]) {
 
 //now we define each function;
 
-searchConcert () {
+function searchConcert () {
+  
+    //we create variable to hold the bands in town URL then run an axios request to Bands in Town API, then print the required details;
+    var bandsURL = "https://rest.bandsintown.com/artists/" + UserInput + "/events?app_id=codingbootcamp";
 
-}
+    axios.get( bandsURL ).then( function ( response ) {
+       console.log( response );
+        // console.log( "Venue: " + response.venue)
+    })
+
+
+};
+
+
+// function searchMovie () {
+//     // we run axios request 
+// }
